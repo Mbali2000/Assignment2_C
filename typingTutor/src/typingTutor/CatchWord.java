55c15fac6e3759b5fe70bb553a565af80be4b3e9
Mbali2000 <107546127+Mbali2000@users.noreply.github.com>
2022-08-26 10:05:14 +0200

    updates

--- typingTutor/src/typingTutor/CatchWord.java
+++ typingTutor/src/typingTutor/CatchWord.java
@@ -7,7 +7,7 @@ import java.util.concurrent.atomic.AtomicBoolean;
 //Thread to monitor the word that has been typed.
 public class CatchWord extends Thread {
 	String target;
-	//FallingWord [] duplicate; //array to hold duplicate words
+	FallingWord [] duplicate = new FallingWord[words.length]; //array to hold duplicate words
 	FallingWord g;
 	FallingWord tempWord = null;
 	static AtomicBoolean done ; //REMOVE
@@ -37,34 +37,45 @@ public class CatchWord extends Thread {
 	
 	public void run() {
 		int i=0;
+		int j = 0;
 		while (i<noWords) {		
 			while(pause.get()) {};
 			//check for duplicate before executing matchWord method
 
 			if(words[i].getWord().equals(target)){
-				tempWord = words[i];
+				duplicate[i] = words[i];
 
-				if(tempWord != null && tempWord.getY() < words[i].getY()){
-					tempWord = words[i];
-
+				for(FallingWord word : duplicate){
+					if(word != null && word.getY() < words[i].getY()){
+						tempWord = words[i];
+	
+					}
 				}
+				//j++;
 			}else{
 				tempWord = words[i];
 			}
-			 g = words[i];
+		System.out.println("hap");
+		
+		/*if(tempWord.matchWord(target)){
+			System.out.println( " score! '" + target); //for checking
+			score.caughtWord(target.length());	
+			//FallingWord.increaseSpeed();
+			break;	
 			
+		}*/
 			
 			i++;
 		}
-		 if(tempWord != null){ g = tempWord;}
-		if(g.matchWord(target)){
-			//tempWord.resetWord();
+		System.out.println("penin");
+		if(words[i].matchWord(tempWord.getWord())){
 		System.out.println( " score! '" + target); //for checking
 		score.caughtWord(target.length());	
 		//FallingWord.increaseSpeed();
 		//break;	
 		
 		}
+		
 	}	
 		
 		
